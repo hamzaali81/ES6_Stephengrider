c0ec6486a350f42b814a46c5b30906c1c8640d17
Vidit Shah <vidit0210@gmail.com>
2019-02-04 23:57:41 +0530

    ForEach

---
+++ forEach/1.js
@@ -0,0 +1,7 @@
+let colors = ['red', 'blue', 'green'];
+for (let i = 0; i < colors.length; i++) {
+    console.log(colors[i]);
+}
+// For Loops -- ES5 method
+console.log('---------------------------------')
+//ES6 method
