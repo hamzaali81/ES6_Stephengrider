1a7d244cf394fca728d36465cc7a807519ef0b5c
Vidit Shah <vidit0210@gmail.com>
2019-03-17 23:57:38 +0530

    V2

--- Every_Some/2es.js
+++
@@ -1,9 +0,0 @@
-let names = ['Vidit', 'Shah', 'Rahul']
-let every = names.every((name) => {
-    return name.length > 4
-})
-console.log(every)
-let some = names.some((name) => {
-    return name.length > 4
-})
-console.log(some)
--- forEach/tempCodeRunnerFile.js
+++
@@ -1,3 +0,0 @@
-numbers.forEach(function (number) {
-//     sum += number;
-// })
