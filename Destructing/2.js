b11ea9d7950befb4ec3eb93d6fa54f63074298a8
Vidit Shah <vidit0210@gmail.com>
2019-03-27 23:34:15 +0530

    desv2

--- Destructing/2.js
+++ Destructing/2.js
@@ -1,2 +1,20 @@
 //Destrucitng Arrays and Objects together
 
+const companies  = [
+    {name:'Google',location:'Mountain View'},
+    {name:'Uber',location:'Down SanFransisco'},
+    {name:'Faceboon',location:'Menlo Park'}
+]
+
+// let [location] = companies;
+// console.log(location)
+// OUTPUT:-{ name: 'Google', location: 'Mountain View' }
+// let [{location}] = companies;
+// console.log(location)
+// // Output : Mountain View
+
+const Google ={
+    locations :['Mountain View','New york']
+}
+const {locations:[location]} =Google
+console.log(location)
